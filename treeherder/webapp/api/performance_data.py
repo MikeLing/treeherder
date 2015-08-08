a8edd917f79e929b8c8cacad51bc6a715cc85491
Tiramisu 1993 <sabergeass@gmail.com>
2015-08-08 10:50:30 +0800

    Add get_platforms endpoint

--- treeherder/webapp/api/performance_data.py
+++ treeherder/webapp/api/performance_data.py
@@ -111,3 +111,23 @@ class PerformanceDataViewSet(viewsets.ViewSet):
         data = jm.get_performance_series_from_signatures(signatures, interval_seconds)
 
         return Response(data)
+
+    @list_route()
+    @with_jobs
+    def platforms(self, request, project, jm, pk=None):
+        """
+
+        :param request:
+        :param project:
+        :param jm:
+        :param pk:
+        :return:
+        """
+        try:
+            interval_second = int(request.QUERY_PARAMS.get('interval'))
+        except:
+            return Response("incorrect parameters", 400)
+
+        platforms = jm.get_platforms(interval_second)
+
+        return Response(platforms)
